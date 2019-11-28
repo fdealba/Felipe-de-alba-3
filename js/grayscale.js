e687a94b5b2659a90c6e958f693c21fb784d1414
felipe de alba <felipe.de.alba.alegre@gmail.com>
2019-11-28 20:26:35 +0100

    asdfasd

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,5 +41,43 @@
 
 })(jQuery); // End of use strict
 
-particlesJS("particles-js", {"particles":{"number":{"value":150,"density":{"enable":true,"value_area":800}},"color":{"value":"#000000"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":3},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":false,"anim":{"enable":false,"speed":0.5,"opacity_min":0.1,"sync":false}},"size":{"value":4.008530152163807,"random":true,"anim":{"enable":true,"speed":5,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":100,"color":"#000000","opacity":0.4,"width":1},"move":{"enable":true,"speed":2,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":140,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+particlesJS("particles-js", {"particles":{"number":{"value":150,"density":{"enable":true,"value_area":800}},"color":{"value":"#1D1D1D"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":3},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":false,"anim":{"enable":false,"speed":0.5,"opacity_min":0.1,"sync":false}},"size":{"value":4.008530152163807,"random":true,"anim":{"enable":true,"speed":5,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":100,"color":"#1D1D1D","opacity":0.4,"width":1},"move":{"enable":true,"speed":2,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":140,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
+
+// When the DOM is loaded and ready, let's do some shit!
+$(document).ready(function() {
+  function skillSet() {
+    // Iterate over each element w/ a class of
+    // bar-info, storing the value of data-total
+    // in a variable.  Using jQuery's CSS method,
+    // dynamically update the width of each bar.
+    $('.bar-info').each(function() {
+      total = $(this).data("total");
+      $(this).css("width", total + "%");
+    });
+
+    // Iterate over each element w/ the class percent.  Using
+    // jQuery's $(this) will allow us to interact w/ each specific
+    // object in the loop.  Then use jQuery's super awesome animate method
+    // to implement a counter on each .percent element, which will "count"
+    // up incrementally until it reaches the number inside the percent span,
+    // aka it's "ceiling".
+    $('.percent').each(function() {
+      var $this = $(this);
+      $({
+        Counter: 10
+      }).animate({
+        Counter: $this.text()
+      }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function() {
+          $this.text(Math.ceil(this.Counter) + "%");
+        }
+      });
+    });
+  };
+  // Invoke our skillSet function inside a setTimeout,
+  // to create a short delay before the animation begins.
+  setTimeout(skillSet, 1000);
+});
